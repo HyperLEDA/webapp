d971b4b83b9a345c868415e29c31b805a0bbdd55
Artyom Zaporozhets <kraysent@gmail.com>
2025-08-30 13:42:58 +0300

    move tooltip up

--- src/components/ui/hint.tsx
+++ src/components/ui/hint.tsx
@@ -10,10 +10,10 @@ interface HintProps {
 
 export const Hint: React.FC<HintProps> = ({ children, hintContent, className = "" }) => {
     return (
-        <div className={`relative flex items-center justify-center gap-2 ${className}`}>
+        <div className={`flex items-center justify-center gap-2 ${className}`}>
             <div>{children}</div>
-            <div className="flex gap-2">
-                <Tooltip content={hintContent} arrow={false} placement="top" className="bg-gray-600 px-2 border-1">
+            <div>
+                <Tooltip content={hintContent} arrow={false} placement="top" className="bg-gray-600 px-2 border-1 max-w-xl">
                     <MdHelpOutline />
                 </Tooltip>
             </div>
