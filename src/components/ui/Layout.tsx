74036d1d11daf0f8ff9e780998cc367665958fb6
kraysent <kraysent@gmail.com>
2026-02-22 15:53:58 +0000

    smoother page transitions

--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,11 +1,14 @@
+import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export function Layout() {
   return (
     <div className="min-h-screen flex">
       <Navbar />
       <div className="ml-12 flex flex-col flex-grow min-h-screen">
-        <div className="flex-grow p-8">{children}</div>
+        <div className="flex-grow p-8">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
