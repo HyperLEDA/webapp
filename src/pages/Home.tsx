50fd6ead6e3de701239e1f9f43d65f2ebf32d63b
kraysent <kraysent@gmail.com>
2026-08-02 22:48:09 +0100

    real examples

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ const homePageHint: ReactElement = (
       </li>
       <li>
         By coordinates (J2000 packed):{" "}
-        <Link href="/query?q=J001122.33%2B443322.1">J001122.33+443322.1</Link>
+        <Link href="/query?q=J123049.42%2B122328.0">J123049.42+122328.0</Link>
       </li>
       <li>
         By coordinates (decimal degrees):{" "}
@@ -18,15 +18,15 @@ const homePageHint: ReactElement = (
       </li>
       <li>
         By coordinates (B1950):{" "}
-        <Link href="/query?q=B123456.7-012345.6">B123456.7-012345.6</Link>
+        <Link href="/query?q=B123049.4%2B122328">B123049.4+122328</Link>
       </li>
       <li>
         By coordinates (galactic):{" "}
-        <Link href="/query?q=G187.7059%2B12.3911">G187.7059+12.3911</Link>
+        <Link href="/query?q=G283.777%2B74.491">G283.777+74.491</Link>
       </li>
       <li>
         By coordinates (supergalactic):{" "}
-        <Link href="/query?q=S123.45-01.23">S123.45-01.23</Link>
+        <Link href="/query?q=S102.89-2.35">S102.89-2.35</Link>
       </li>
     </ul>
   </div>
